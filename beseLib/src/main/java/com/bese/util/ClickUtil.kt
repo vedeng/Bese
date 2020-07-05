6df17bb3cb90be8139cb4ebfac12499a597c42d8
BD <it8098@vedeng.com>
2020-07-05 20:10:43 +0800

    TT1

--- beseLib/src/main/java/com/bese/util/ClickUtil.kt
+++ beseLib/src/main/java/com/bese/util/ClickUtil.kt
@@ -5,26 +5,28 @@ package com.bese.util
  */
 object ClickUtil {
 
+    private const val SLOW_DIFF: Long = 1600
     private const val DIFF: Long = 500
     private var lastClickTime: Long = 0
     private var lastButtonId = -1
+    private var lastClickTimeToQuitApp: Long = 0
 
     /**
      * 判断两次点击的间隔，如果小于DIFF，则认为是多次无效点击
      * @return 是否双击
      */
-    fun filterGlobal(diff: Long = DIFF): Boolean {
-        return filter(-1, diff)
-    }
-
-    fun filter(buttonId: Int?, diff: Long = DIFF) : Boolean {
-        return isFastDoubleClick(buttonId, diff)
+    fun isFastDoubleClick(): Boolean {
+        return isFastDoubleClick(-1, DIFF)
     }
 
     /**
      * 判断两次点击的间隔，如果小于diff，则认为是多次无效点击
+     *
+     * @param buttonId 控件
+     * @param diff 双击判定间隔
+     * @return
      */
-    fun isFastDoubleClick(buttonId: Int?, diff: Long = DIFF): Boolean {
+    private fun isFastDoubleClick(buttonId: Int?, diff: Long = DIFF): Boolean {
         val time = System.currentTimeMillis()
         val timeD = time - lastClickTime
         if (lastButtonId == buttonId && lastClickTime > 0 && timeD < diff) {
@@ -35,5 +37,24 @@ object ClickUtil {
         return false
     }
 
+    /**
+     * 判断两次点击的间隔，如果小于diff，则认为是多次无效点击
+     *
+     * @param buttonId 控件
+     * @param diff 双击判定间隔
+     * @return
+     */
+    fun isSlowDoubleClick(buttonId: Int?): Boolean {
+        return isFastDoubleClick(buttonId, SLOW_DIFF)
+    }
 
+    /**
+     * 双击退出APP操作
+     */
+    fun clickToQuitApp(): Boolean {
+        val time = System.currentTimeMillis()
+        val oldTime = lastClickTimeToQuitApp
+        lastClickTimeToQuitApp = time
+        return time - oldTime <= 2000
+    }
 }
